fbca0f77594dcaca5f86eb57356e5f69aa2c0c04
Recep Alışkan <recep.aliskan@devatek.com>
2021-10-26 22:33:15 +0300

    Add project files.

---
+++ FindCharDifference/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+FindCharDifference();
+
+void FindCharDifference()
+{
+    string str = "AAABAB";
+
+    char[] charArr = str.ToCharArray();
+
+    int countA = 0;
+    int countB = 0;
+
+    foreach (char ch in charArr)
+    {
+        if (ch == 'A')
+        {
+            countA++;
+        }
+        else
+        {
+            countB++;
+        }
+    }
+
+    Console.WriteLine(Math.Abs(countA - countB));
+}
+
---
+++ FindOddNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+FindOddNumber();
+
+void FindOddNumber()
+{
+    int[] list = new int[] { 1, 3, 5, 7, 9, 13, 15, 17 };
+
+    var minValue = list.ToList().OrderBy(f => f).FirstOrDefault();
+    int result = 0;
+
+    while (true)
+    {
+        if (list.Contains(minValue))
+        {
+            minValue += 2;
+        }
+        else
+        {
+            result = minValue;
+            break;
+        }
+    }
+
+    Console.WriteLine(result);
+}
+
+
+
