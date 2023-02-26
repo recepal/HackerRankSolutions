575af95c55ff5d1b5f6702a3b4a7269d74c7c968
Recep Alışkan <recepaliskan1@gmail.com>
2023-02-26 16:32:25 +0300

    practices

---
+++ CountingValleys/Program.cs
@@ -0,0 +1,27 @@
+﻿CountingValleys(8, "UDDDUDUU");
+
+void CountingValleys(int steps, string path)
+{
+    int result = 0;
+    var listPaths = path.ToCharArray().ToList();
+    int seaLevel = 0;
+    for (int i = 0; i < steps; i++)
+    {
+        if (listPaths[i] == 'U')
+        {
+            seaLevel++;
+
+            if (seaLevel == 0)
+            {
+                result++;
+            }
+        }
+        else
+        {
+            seaLevel--;
+        }
+    }
+
+    Console.WriteLine(result);
+}
+
---
+++ DivisibleSumPairs/Program.cs
@@ -0,0 +1,18 @@
+﻿DivisibleSumPairs(6, 3, new List<int>() { 1, 3, 2, 6, 1, 2 });
+
+void DivisibleSumPairs(int n, int k, List<int> ar)
+{
+    int result = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if ((ar[i] + ar[j]) % k == 0)
+            {
+                result++;
+            }
+        }
+    }
+
+    Console.WriteLine(result);
+}
